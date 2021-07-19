7f36ea019f169e4ff6a2ea7f7533a3758bda35bb
Kyle Butler <54778108+Kyle9021@users.noreply.github.com>
2021-07-19 18:06:23 -0400

    Create uninstall_ubuntu_20_04_iac_gui_desktop_tool.sh

---
+++ uninstall_ubuntu_20_04_iac_gui_desktop_tool.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo rm /usr/local/bin/iac_script
+sudo rm /home/$USER/Desktop/Prisma_IT_IaC_Scan_Tool.desktop
+sudo rm ~/.local/share/applications/Prisma_IT_IaC_Scan_Tool.desktop
+echo "uninstall complete"
